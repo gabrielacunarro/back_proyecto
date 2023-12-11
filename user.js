b099ac4a4bc1c678633d7ece072f8e0e99650436
Gabriela Cuñarro <gabrielacunarro@gmail.com>
2023-12-11 20:21:36 -0300

    nueva rama

--- user.js
+++ user.js
@@ -1,13 +1,25 @@
 class UserManager {
     #users = [];
+    
     create(data) {
-        const user = {
-            id: this.#users.length === 0 ? 1 : this.#users[this.#users.length - 1].id + 1,
-            name: data.name,
-            photo: data.photo,
-            email: data.email
-        };
-        this.#users.push(user);
+        const requiredProps = ["name", "photo", "email"];
+
+        //  verifico si estan todas las propiedades de cada user, caso contrario arrojo un advertencia/error de creacion del mismo
+        const missingProps = requiredProps.filter(prop => !(prop in data) || data[prop] === undefined);
+
+        if (missingProps.length > 0) {
+            const missingMessages = missingProps.map(prop => `The user has not been created as the "${prop}" property is missing.`);
+            console.log(`Warning: ${missingMessages.join(". ")}`);
+            
+        } else {
+            const user = {
+                id: this.#users.length === 0 ? 1 : this.#users[this.#users.length - 1].id + 1,
+                name: data.name,
+                photo: data.photo,
+                email: data.email
+            };
+            this.#users.push(user);
+        }
     }
 
     read() {
@@ -23,17 +35,17 @@ const userManager = new UserManager();
 userManager.create({
     name: "Alejandro Perez",
     photo: "Alejandro.jpg",
-    email: "alejandro.perez@example.com"
+    email: "alejandro.perez@gmail.com"
 });
 userManager.create({
     name: "Federico Suarez",
     photo: "Federico.jpg",
-    email: "federico_suarez@example.com"
+    email: "federico_suarez@gmail.com"
 });
 userManager.create({
     name: "Ana De Luca",
     photo: "Ana.jpg",
-    email: "ana_deluca@example.com"
+    email: "ana_deluca@gmail.com"
 });
 userManager.create({
     name: "Maria Sosa",
@@ -43,3 +55,4 @@ userManager.create({
 
 console.log("Users:", userManager.read());
 console.log("User with id 2:", userManager.readOne(2));
+
