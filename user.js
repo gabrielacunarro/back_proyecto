ea0a2008fe9ec9437bb36d98e904bdaac8cf99e9
Gabriela Cuñarro <gabrielacunarro@gmail.com>
2023-12-17 16:31:27 -0300

    agrego json

--- user.js
+++ user.js
@@ -1,5 +1,9 @@
+const fs = require('fs').promises;
+const path = require('path');
+
 class UserManager {
-    #users = [];
+    static #usersFile = path.resolve(__dirname, 'data', 'users.json');
+    static #users = [];
 
     #verifyRequiredProps(data) {
         const requiredProps = ["name", "photo", "email"];
@@ -8,7 +12,7 @@ class UserManager {
     }
 
     #generateUserId() {
-        return this.#users.length === 0 ? 1 : this.#users[this.#users.length - 1].id + 1;
+        return UserManager.#users.length === 0 ? 1 : UserManager.#users[UserManager.#users.length - 1].id + 1;
     }
 
     #generateWarningMessage(missingProps) {
@@ -16,7 +20,7 @@ class UserManager {
         return `Warning: ${missingMessages.join(". ")}`;
     }
 
-    create(data) {
+    async create(data) {
         const missingProps = this.#verifyRequiredProps(data);
 
         if (missingProps.length > 0) {
@@ -28,20 +32,62 @@ class UserManager {
                 photo: data.photo,
                 email: data.email
             };
-            this.#users.push(user);
+            UserManager.#users.push(user);
+
+            try {
+                await fs.access(dataFolder);
+            } catch (error) {
+                // Si la carpeta no existe, la crea
+                try {
+                    await fs.mkdir(dataFolder);
+                } catch (mkdirError) {
+                    console.error('Error creating folder:', mkdirError.message);
+                }
+            }
+
+            await this.saveUsers();
         }
     }
 
     read() {
-        return this.#users;
+        return UserManager.#users;
     }
 
     readOne(id) {
-        return this.#users.find(user => user.id === Number(id));
+        return UserManager.#users.find(user => user.id === Number(id));
+    }
+
+    async loadUsers() {
+        try {
+            const data = await fs.readFile(UserManager.#usersFile, 'utf8');
+            if (data.trim() === '') {
+                // Inicializo #users como un array vacío
+                UserManager.#users = [];
+            } else {
+                UserManager.#users = JSON.parse(data);
+            }
+        } catch (error) {
+            // Manejo de error creando un user vacío
+            console.error('Error loading users:', error.message);
+            UserManager.#users = [];
+        }
+    }
+
+    async saveUsers() {
+        try {
+            const data = JSON.stringify(UserManager.#users, null, 2);
+            await fs.writeFile(UserManager.#usersFile, data, 'utf8');
+        } catch (error) {
+            console.error('Error saving users:', error.message);
+        }
     }
 }
 
+// creo la carp 'data' para almacenar el archivo JSON
+const dataFolder = path.join(__dirname, 'data');
+
 const userManager = new UserManager();
+
 userManager.create({
     name: "Alejandro Perez",
     photo: "Alejandro.jpg",
@@ -52,11 +98,13 @@ userManager.create({
     photo: "Federico.jpg",
     email: "federico_suarez@gmail.com"
 });
+
 userManager.create({
     name: "Ana De Luca",
     photo: "Ana.jpg",
     email: "ana_deluca@gmail.com"
 });
+
 userManager.create({
     name: "Maria Sosa",
     photo: "Maria.jpg",
@@ -64,4 +112,5 @@ userManager.create({
 });
 
 console.log("Users:", userManager.read());
-console.log("User with id 2:", userManager.readOne(1));
+console.log("User with ID 1:", userManager.readOne(1));
+
